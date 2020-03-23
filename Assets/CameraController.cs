153cfac78410c138c405b03a4bd02438ae17bc33
thejovialknight <thejovialknight@gmail.com>
2020-03-22 18:13:00 -0700

    Started Camera Controller

---
+++ Assets/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Vector2 position;
+    public int zoomLevel;
+    public List<float> zoomLevels = new List<float>();
+
+    void Update()
+    {
+        
+    }
+}
