7c9ff817f57e24c4fbcfbaa7e28e675555653fdb
thejovialknight <thejovialknight@gmail.com>
2020-03-24 22:45:18 -0700

    Started Mail Sort Minigame
    Mostly art assets

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -6,8 +6,11 @@ public class CameraController : MonoBehaviour
 {
     Camera cam;
     float desiredPerfectSize;
+    public bool immediateTrigger = true;
 
     public CameraZone currentZone;
+    public CameraZone overrideZone;
+    public bool isOverride = false;
     public float lerpFactor = 2.0f;
 
 
@@ -21,15 +24,40 @@ public class CameraController : MonoBehaviour
         currentZone = zone;
     }
 
+    public void StartZoneOverride(CameraZone zone, bool isImmediate) {
+        isOverride = true;
+        overrideZone = zone;
+
+        if(isImmediate) {
+            immediateTrigger = true;
+        }
+    }
+
+    public void EndZoneOverride(bool isImmediate) {
+        isOverride = false;
+
+        if(isImmediate) {
+            immediateTrigger = true;
+        }
+    }
+
     void Update()
     {
+        CameraZone zoneToUse;
+        if(isOverride) {
+            zoneToUse = overrideZone;
+        }
+        else {
+            zoneToUse = currentZone;
+        }
+
         float pixelRatio = 1.0f;
         while(true) 
         {
             desiredPerfectSize = ((Screen.height)/(pixelRatio * 16.0f)) * 0.5f;
 
             // Once too small, scale it up
-            if(desiredPerfectSize < currentZone.desiredHeight / 2f) 
+            if(desiredPerfectSize < zoneToUse.desiredHeight / 2f) 
             {
                 desiredPerfectSize = ((Screen.height)/((pixelRatio - 1.0f) * 16.0f)) * 0.5f;
                 break;
@@ -37,6 +65,12 @@ public class CameraController : MonoBehaviour
             pixelRatio = pixelRatio + 1.0f;
         }
         cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, desiredPerfectSize, Time.deltaTime * lerpFactor);
-        cam.transform.position = Vector3.Lerp(cam.transform.position, new Vector3(currentZone.position.x, currentZone.position.y, -10.0f), Time.deltaTime * lerpFactor);
+        cam.transform.position = Vector3.Lerp(cam.transform.position, new Vector3(zoneToUse.position.x, zoneToUse.position.y, -10.0f), Time.deltaTime * lerpFactor);
+
+        if(immediateTrigger) {
+            immediateTrigger = false;
+            cam.orthographicSize = desiredPerfectSize;
+            cam.transform.position = new Vector3(zoneToUse.position.x, zoneToUse.position.y, -10.0f);
+        }
     }
 }
---
+++ Assets/DoorController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class DoorController : MonoBehaviour, IInteractable
+{
+    SpriteRenderer spriteRenderer;
+    BoxCollider2D boxCollider;
+
+    public Sprite closedSprite;
+    public Sprite openSprite;
+    public bool isOpen = false;
+    public string doorName = "Door";
+
+    void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    void Start() {
+        ValidateOpen();
+    }
+
+    void OnValidate() {
+        ValidateOpen();
+    }
+
+    public void InteractWith(Transform interactor) {
+        if(isOpen) {
+            isOpen = false;
+        }
+        else 
+        {
+            isOpen = true;
+        }
+        ValidateOpen();
+    }
+    public string GetInteractName() {
+        return doorName;
+    }
+    public string GetInteractInfo() {
+        return "Press E to open";
+    }
+
+    void ValidateOpen() {
+        if(isOpen) {
+            spriteRenderer.sprite = openSprite;
+            boxCollider.offset = new Vector2(-0.25f, -0.8125f);
+            boxCollider.size = new Vector2(0.125f, 0.75f);
+        }
+        else 
+        {
+            spriteRenderer.sprite = closedSprite;
+            boxCollider.offset = new Vector2(0.0f, -0.5f);
+            boxCollider.size = new Vector2(0.625f, 0.125f);
+        }
+    }
+}
